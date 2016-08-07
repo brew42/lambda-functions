ff21b50c3a40c3408993b85426172df4d808e530
paulSambolin <paulsambolin@gmail.com>
2016-08-07 14:31:13 -0400

    refactored index.js to use promises

--- populate-rds/index.js
+++ populate-rds/index.js
@@ -6,7 +6,9 @@ var fs = require('fs');
 var unzip = require('unzip');
 var async = require('async');
 
-exports.handler = function(event, context,callback) {
+var script = "./pgsql/bin/psql -f /tmp/sweetskills/create_master.sql --dbname=postgresql://";
+
+exports.handler = (event, context,callback) => {
 
 	console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
 
@@ -16,8 +18,6 @@ exports.handler = function(event, context,callback) {
     }
 
     var responseData = {};
-    var script = "./pgsql/bin/psql -f /tmp/sweetskills/create_master.sql --dbname=postgresql://";
-
     var DBMasterUserName = event.ResourceProperties.DBMasterUserName;
     var DBMasterPassword = event.ResourceProperties.DBMasterPassword;
     var DBAddress = event.ResourceProperties.DBAddress;
@@ -26,58 +26,55 @@ exports.handler = function(event, context,callback) {
     
     script = script + DBMasterUserName + ":" + DBMasterPassword + "@" + DBAddress + ":" + DBListeningPort + "/" + DBName;
 
-    const exec = require('child_process').exec;
-
-    async.series([
-        function download(downloadNext) {
-            var s3 = new AWS.S3();
-            s3.getObject({
-                Bucket: "honey-badger-data",
-                Key: "sweetskills.zip"
-            }).createReadStream().pipe(fs.createWriteStream('/tmp/sweetskills.zip')).on('finish', function(err) {
-                if (err) {
-                    console.log('error download');
-                    downloadNext(err);
-                } else {
-                    downloadNext(null);
-                }
-            });
-        },
-        function open(openNext) {
-            fs.createReadStream('/tmp/sweetskills.zip').pipe(unzip.Extract({ path: '/tmp' }).on('close', function(err) {
-                if (err) {
-                    console.log('error open');
-                    openNext(err);
-                } else {
-                    openNext(null);
-                }
-            }));
-        },
-        function execute3(executeNext) {
-            const child3 = exec(script, (error, stdout, stderr) => {
-                if (error) {
-                    console.log("Error execute script");
-                    console.log(error);
-                    response.send(event, context, response.FAILED, responseData);
-                }
-                console.log(stdout);
-                console.log(stderr);
-                executeNext(null);
-            });
-        },
-        function respond(respondNext) {
+    download()
+        .then(open)
+        .then(execute)
+        .then(() => {
             response.send(event, context, response.SUCCESS, responseData);
-            respondNext(null);
-        }
-    ], function (error) {
-        if (error) {
-            console.log('Error- failed');
-            console.log(error);
-        }
+        })
+        .catch((err) => {
+            context.done(err);
+        });
+}
+
+function download() {
+    var s3 = new AWS.S3();
+    return new Promise((resolve) => {
+        s3.getObject({
+            Bucket: "honey-badger-data",
+            Key: "sweetskills.zip"
+        })
+        .createReadStream()
+        .pipe(fs.createWriteStream('/tmp/sweetskills.zip')
+            .on('close', () => {
+                    resolve();
+            })
+        );
     });
 }
 
+function open() {
+    return new Promise((resolve) => {
+        fs.createReadStream('/tmp/sweetskills.zip')
+        .pipe(unzip.Extract({ path: '/tmp' })
+            .on('close', () => {
+                    resolve();
+            })
+        );
+    });
+}
 
-
-
-
+function execute() {
+    console.log(script);
+    const exec = require('child_process').exec;
+    return new Promise((resolve) => {
+        const child = exec(script, (err, stdout, stderr) => {
+            if(err) {
+                console.log(err);
+            }
+            console.log(stdout);
+            console.log(stderr);
+            resolve();
+        });
+    });
+}
