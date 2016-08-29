afae49287a0262cb8909e765417b08ae23975b70
paulSambolin <paulsambolin@gmail.com>
2016-08-29 11:40:50 -0400

    moved respone.js out of node_modules and refactored with promises

--- empty-s3-bucket/index.js
+++ empty-s3-bucket/index.js
@@ -1,24 +1,30 @@
 'use strict'
-var response = require('response.js');
+var response = require('./response.js');
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3({
     region: 'us-east-1'
 });
+
+var globalBucketName = '';
 
 exports.handler = (event, context, callback) => {
 
     console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
 
     if (event.RequestType == 'Delete') {
-        emptyBucket(event.ResourceProperties.BucketName,
-          (err) => {
-            if(err) {
-                console.log("error: ", err);
-                response.send(event, context, response.SUCCESS);
-            }
-            else {
-              response.send(event, context, response.SUCCESS);
-            }
+      globalBucketName = event.ResourceProperties.BucketName;
+
+      getObjects()
+        .then((data) => {
+            emptyBucket(data);
+        })
+        .then(function () {
+          response.send(event, context, response.SUCCESS);
+          return;
+        })
+        .catch(function(err) {
+          return;
+          console.log(err);
         });
     }
     else { 
@@ -27,41 +33,24 @@ exports.handler = (event, context, callback) => {
     }  
 };
 
-
-function emptyBucket(bucketName,callback){
+function getObjects(){
   var params = {
-    Bucket: bucketName,
+    Bucket: globalBucketName,
   };
   console.log("Bucket: ", params.Bucket);
-  s3.listObjectsV2(params, function(err, data) {
-    if (err) {
-      return callback(err);
-    }
-    else if (data.Contents.length == 0) {
-      return callback();
-    }
-    else {
-      params = {Bucket: bucketName};
-      params.Delete = {Objects:[]};
+  return s3.listObjectsV2(params).promise();
+}
 
-      data.Contents.forEach(function(content) {
-        console.log("Deleting Object: ", content.Key);
-        params.Delete.Objects.push({Key: content.Key});
-      });
+function emptyBucket(data){
+  var params = {Bucket: globalBucketName};
+  params.Delete = {Objects:[]};
 
-      s3.deleteObjects(params, function(err, data) {
-        if (err) {
-          return callback(err);
-        }
-        else if(data.Deleted.length == 1000) {
-          emptyBucket(bucketName,callback);
-        }
-        else {
-          return callback();
-        }
-      });
-    }
+  data.Contents.forEach(function(content) {
+    console.log("Deleting Object: ", content.Key);
+    params.Delete.Objects.push({Key: content.Key});
   });
+
+  return s3.deleteObjects(params).promise();
 }
 
 
